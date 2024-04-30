4b112b7bbb8e068039b2c901962492b679e25870
Genilso Carraro <71860731+Genilso190@users.noreply.github.com>
2024-04-30 03:22:31 +0000

    fix: Landscap

---
+++ app/components/Landscap.tsx
@@ -0,0 +1,33 @@
+
+
+export default function Landscap(){
+    return(
+        <> <div className="mt-6 gap-2 grid grid-cols-2 justify-items-center lg:flex justify-between">  
+                <div className="">
+                 <a href="">
+                 <img src="/img/tour.png1" alt="" />
+                 </a>
+                </div>
+
+                <div>
+                    <a href="">
+                    <img src="/img/tour.png2" alt="" />
+                    </a>
+                </div>
+
+                <div className="">
+                    <a href="">
+                    <img src="/img/tour.png3" alt="" />
+                    </a>
+                </div>
+
+                <div className="">
+                    <a href="">
+                    <img src="/img/tour.png4" alt="" />
+                    </a>
+                </div>
+            </div>
+        </>
+    )
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import Landscap from "./components/Landscap";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Banner/>
+      <Landscap/>
     </div>
  
   )
