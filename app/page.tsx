4a8beaa439c25f452de1608a9b4237d302ae2f34
Genilso Carraro <71860731+Genilso190@users.noreply.github.com>
2024-04-30 02:02:33 +0000

    fix: Banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import Banner from "./components/Banner";
 import Header from "./components/Header";
-
 
 export default function Home() {
   return (
-    <div className="bg-white">
-      <Header/>
+    <div>
+      <Header />
+      <Banner/>
     </div>
+ 
   )
-    
 }
+
+
